6272f3ab96445fe1d890f23b9252bed4cfb53b48
YeYint3424 <xino3424@gmail.com>
2024-07-19 10:27:41 +0630

    fixed

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
         <h1 className="navText">Pick A Pile</h1>
       </Link>
       <a
-        href="https://646e-65-18-112-62.ngrok-free.app/payment"
+        href="https://3df2-103-116-58-222.ngrok-free.app/payment"
         className="text-pink-300 font-title text-sm"
       >
         Pictures By Hnin❄️
